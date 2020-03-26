b09322c0223e107c102ff6dc94879d12b3285632
nickdujakovich <nickdujakovich@hotmail.com>
2020-03-26 13:19:37 -0700

    first commit

---
+++ c.py
@@ -0,0 +1,31 @@
+# Python program to implement client side of chat room. 
+import socket 
+import select 
+import sys 
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+
+IP_address = "3.12.58.153"
+Port = 12000
+print("Connecting...")
+server.connect((IP_address, Port)) 
+
+while True: 
+
+    sockets_list = [sys.stdin, server] 
+    read_sockets,write_socket,error_socket = select.select(sockets_list,[],[]) 
+
+    for socks in read_sockets: 
+        if socks == server: 
+            message = socks.recv(2048) 
+            print(message)
+        else:
+            message = raw_input()
+            if(message == "{exit}"):
+                server.close()
+            server.send(message)
+            print("You: " + message)
+
+server.close() 
+
---
+++ s.py
@@ -0,0 +1,66 @@
+# Python program to implement server side of chat room. 
+import socket 
+import select 
+import sys 
+from thread import *
+  
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+
+
+
+IP_address = "0.0.0.0"
+print("My IP: " + IP_address)
+Port = 12000
+  
+server.bind((IP_address, Port)) 
+
+server.listen(10) 
+  
+clientlist = [] 
+  
+def clientthread(conn, addr): 
+  
+    conn.send("Welcome to NetChat!")
+    conn.send("Type {exit} to disconnect.\n") 
+  
+    while True: 
+            try: 
+                message = conn.recv(2048) 
+                if message: 
+
+                    print addr[0] + ": " + message 
+                    message_to_send = addr[0] + ": " + message 
+                    broadcast(message_to_send, conn) 
+  
+                else: 
+                    remove(conn) 
+            except: 
+                continue
+  
+def broadcast(message, connection): 
+    for clients in clientlist: 
+        if clients!=connection: 
+            try: 
+                clients.send(message) 
+            except: 
+                clients.close() 
+                remove(clients) 
+  
+def remove(connection): 
+    if connection in clientlist: 
+        print(connection.getpeername()[0] + " on port " + connection.getpeername()[1] + "has disconnected.")
+        list_of_clients.remove(connection) 
+  
+while True: 
+  
+    conn, addr = server.accept() 
+  
+    clientlist.append(conn) 
+  
+    print addr[0] + " connected"
+  
+    start_new_thread(clientthread,(conn,addr))     
+  
+conn.close() 
+server.close() 
